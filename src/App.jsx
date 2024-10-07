0a2ecb2db154c62127a3e9ebea34085fb9d487a1
Tanmay7586 <tanmaydarekar7586@gmail.com>
2024-10-07 15:08:26 +0530

    home page- hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
+import HeroPage from "./components/main-page/heroPage";
+import Yogas from "./components/yoga-poses/Yogas";
 
 function App() {
   return (
-    <>
+    <div className="bg-slate-50">
       <Navbar />
-    </>
+      <HeroPage />
+      <Yogas />
+    </div>
   );
 }
 
---
+++ src/components/yoga-poses/Yogas.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Yogas = () => {
+  return (
+    <div>
+      <div className="mx-auto max-w-7xl py-6">
+        <h1 className="text-center text-indigo-600 font-extrabold text-4xl">Yoga Poses</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Yogas;
